5bfd588899dfdb5d154f39b411d0a85e95f441d0
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-04-03 12:23:50 +0300

    Update Front-end and  Back-end

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
 
 const PORT = 4820;
 const app = express();
@@ -7,8 +9,14 @@ const app = express();
 const corsOptions = {
     origin: 'http://localhost:4819',
 };
+const helmetOptions = {
+    crossOriginResourcePolicy: false,
+};
 
 app.use(cors(corsOptions));
+app.use(helmet(helmetOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     return res.send('Home page');
@@ -17,6 +25,27 @@ app.get('/', (req, res) => {
 app.get('/api/calc', (req, res) => {
     return res.send(JSON.stringify({
         result: 123,
+    }));
+});
+
+app.post('/api/calc', (req, res) => {
+    const data = req.body;
+    const {pirmas, antras} = data;
+    
+    if (isNaN(+pirmas)) {
+        return res.send(JSON.stringify({
+            result: 'Pirmoji reiksme nera skaicius',
+        }));
+    }
+
+    if (isNaN(+antras)) {
+        return res.send(JSON.stringify({
+            result: 'Antroji reiksme nera skaicius',
+        }));
+    }
+
+    return res.send(JSON.stringify({
+        result: (+pirmas) + (+antras),
     }));
 });
 
